807a992e7b348863982daa863ababe7a1746793e
Max Milton <max@wearegenki.com>
2016-01-05 08:39:20 +1100

    Add readme and slight cleanup

---
+++ README.md
@@ -0,0 +1,6 @@
+Ephemeral Google Chrome in a Docker container useful for testing websites. Sometimes it's hard to clean caches (DNS, caching proxies, etc) but this is a browser environment that's completely clean every time.
+
+Overview
+------
+
+TODO
--- launch-chrome.docker.sh
+++ launch-chrome.docker.sh
@@ -5,7 +5,7 @@ xhost +local:docker
 
 # Run chrome in a restrictive Docker container
 # Also block websites for productivity
-exec docker run -i -t --rm \
+exec docker run --rm \
     --cpuset-cpus 0 \
     --memory 1g \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
@@ -13,8 +13,7 @@ exec docker run -i -t --rm \
     --device /dev/snd \
     --volume /dev/snd:/dev/snd \
     --cap-drop=all \
-    --add-host www.youtube.com:127.0.0.1 \
-    --add-host "www.xvideos.com www.yuvutu.com www.youporn.com www.redtube.com www.pornhub.com pornfun.com theporndude.com":127.0.0.1 \
+    --add-host "www.youtube.com ":127.0.0.1 \
     local/chrome
     
 # TODO: Remove --volume /dev/snd when --device works as expected
